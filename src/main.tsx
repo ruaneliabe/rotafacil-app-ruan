54f4cc4c61efbf343c6eda3be67beb8bc5091f6d
ruaneliabe <99041745+ruaneliabe@users.noreply.github.com>
2026-08-10 13:34:14 -0300

    Ativa correcoes de UX e virada diaria

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import {RuntimeCorrections} from './components/RuntimeCorrections';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <RuntimeCorrections />
     <App />
   </StrictMode>,
 );
