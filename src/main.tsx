1f79286c0d40f99a176b6563020124c857fc7948
ruaneliabe <99041745+ruaneliabe@users.noreply.github.com>
2026-09-08 18:37:25 -0300

    fix(cardapio-web): activate shadow status reconciliation

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import {RuntimeCorrections} from './components/RuntimeCorrections';
+import {CardapioWebShadowSyncBridge} from './components/CardapioWebShadowSyncBridge';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RuntimeCorrections />
+    <CardapioWebShadowSyncBridge />
     <App />
   </StrictMode>,
 );
