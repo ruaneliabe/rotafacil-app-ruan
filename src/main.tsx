9dd34fa783aaabd5d9ac4d958e011822264fb231
ruaneliabe <99041745+ruaneliabe@users.noreply.github.com>
2026-09-08 21:06:28 -0300

    fix(ui): enable refresh bootstrap guard

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import {RuntimeCorrections} from './components/RuntimeCorrections';
 import {CardapioWebShadowSyncBridge} from './components/CardapioWebShadowSyncBridge';
+import {StoreBootstrapGuard} from './components/StoreBootstrapGuard';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <StoreBootstrapGuard />
     <RuntimeCorrections />
     <CardapioWebShadowSyncBridge />
     <App />
