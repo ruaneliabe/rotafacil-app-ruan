607ba53bf31d9b280bfecdd1cb2eba0f74f7ffe5
ruaneliabe <99041745+ruaneliabe@users.noreply.github.com>
2026-09-08 21:48:45 -0300

    fix(cardapio-web): enable terminal order cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import {RuntimeCorrections} from './components/RuntimeCorrections';
 import {CardapioWebShadowSyncBridge} from './components/CardapioWebShadowSyncBridge';
+import {CardapioWebTerminalCleanup} from './components/CardapioWebTerminalCleanup';
 import {StoreBootstrapGuard} from './components/StoreBootstrapGuard';
 import './index.css';
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
     <StoreBootstrapGuard />
     <RuntimeCorrections />
     <CardapioWebShadowSyncBridge />
+    <CardapioWebTerminalCleanup />
     <App />
   </StrictMode>,
 );
