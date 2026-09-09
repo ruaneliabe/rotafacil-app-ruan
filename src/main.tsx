22ae0dcbbc501d0c1dfbe4aef86e7d5bdf5a5b54
ruaneliabe <99041745+ruaneliabe@users.noreply.github.com>
2026-09-08 21:54:49 -0300

    feat(ui): enable production dispatch cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import {CardapioWebShadowSyncBridge} from './components/CardapioWebShadowSyncBri
 import {CardapioWebTerminalCleanup} from './components/CardapioWebTerminalCleanup';
 import {StoreBootstrapGuard} from './components/StoreBootstrapGuard';
 import {LiveOperationGuard} from './components/LiveOperationGuard';
+import {DispatchBoardProductionPatch} from './components/DispatchBoardProductionPatch';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')!).render(
     <CardapioWebShadowSyncBridge />
     <CardapioWebTerminalCleanup />
     <LiveOperationGuard />
+    <DispatchBoardProductionPatch />
     <App />
   </StrictMode>,
 );
