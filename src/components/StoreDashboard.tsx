285304f0aa97fb5fdd8502928ede6967f19dc1a4
ruaneliabe <99041745+ruaneliabe@users.noreply.github.com>
2026-09-10 13:43:56 -0300

    fix ui: mount single order action enhancer

--- src/components/StoreDashboard.tsx
+++ src/components/StoreDashboard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { StoreDashboard as LegacyStoreDashboard } from './StoreDashboardLegacy';
 import { TeamManagementPanel } from './TeamManagementPanel';
 import { OperationManagementEnhancer } from './OperationManagementEnhancer';
+import { SingleOrderActionEnhancer } from './SingleOrderActionEnhancer';
 
 export const StoreDashboard: React.FC<any> = (props) => {
   return (
@@ -9,6 +10,7 @@ export const StoreDashboard: React.FC<any> = (props) => {
       <LegacyStoreDashboard {...props} />
       <TeamManagementPanel {...props} />
       <OperationManagementEnhancer {...props} />
+      <SingleOrderActionEnhancer />
     </>
   );
 };
