6b7915f0d13da464c152d79b6c3d103c2277a0c7
ruaneliabe <99041745+ruaneliabe@users.noreply.github.com>
2026-09-10 13:24:45 -0300

    feat(dispatch): mount new live delivery management panel

--- src/components/StoreDashboard.tsx
+++ src/components/StoreDashboard.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StoreDashboard as LegacyStoreDashboard } from './StoreDashboardLegacy';
 import { TeamManagementPanel } from './TeamManagementPanel';
+import { OperationManagementEnhancer } from './OperationManagementEnhancer';
 
 export const StoreDashboard: React.FC<any> = (props) => {
   return (
     <>
       <LegacyStoreDashboard {...props} />
       <TeamManagementPanel {...props} />
+      <OperationManagementEnhancer {...props} />
     </>
   );
 };
