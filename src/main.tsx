e2049bc307674b8c4a4920cbd4270714acc6bbdf
ruaneliabe <99041745+ruaneliabe@users.noreply.github.com>
2026-09-10 12:05:56 -0300

    fix(pilot): remove obsolete visual patch from current Claude layout

--- src/main.tsx
+++ src/main.tsx
@@ -7,14 +7,12 @@ import {CardapioWebTerminalCleanup} from './components/CardapioWebTerminalCleanu
 import {StoreBootstrapGuard} from './components/StoreBootstrapGuard';
 import {LiveOperationGuard} from './components/LiveOperationGuard';
 import {DispatchBoardProductionPatch} from './components/DispatchBoardProductionPatch';
-import {PilotVisualPolish} from './components/PilotVisualPolish';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <StoreBootstrapGuard />
     <RuntimeCorrections />
-    <PilotVisualPolish />
     <CardapioWebShadowSyncBridge />
     <CardapioWebTerminalCleanup />
     <LiveOperationGuard />
